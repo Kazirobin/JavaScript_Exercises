5ef014076d35e54dbb4cdde55ed3575c2cfbdd5b
Kazi Robin <kazirobinking@gmail.com>
2023-10-13 22:30:41 +0600

    Update(13/10/23)

--- Sound_Board/script.js
+++ Sound_Board/script.js
@@ -1,15 +1,19 @@
+// array for text and id 
 const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
-
+// use forEach() for select all item in array 
 sounds.forEach(sound => {
     const btn = document.createElement('button');
     btn.classList.add('btn');
+    // array text declare in all btn  
     btn.innerText = sound ;
+    // give event to all btn 
     btn.addEventListener('click',() => {
         stopSongs()
         document.getElementById(sound).play();
     })
     document.getElementById('buttons').appendChild(btn)
 })
+// sound in btn function 
 function stopSongs(){
     sounds.forEach(sound => {
         const song = document.getElementById(sound);
